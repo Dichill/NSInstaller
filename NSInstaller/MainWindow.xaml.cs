7429eede54f93700e037b1322340e55efebaa051
Dichill <dichilltomarong@yahoo.com>
2021-10-04 02:05:01 +0800

    first commit

---
+++ NSInstaller/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using AdonisUI.Controls;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using NSInstaller.Utils;
+using NSInstaller.Ookii;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NSInstaller
+{
+    public partial class MainWindow : AdonisWindow
+    {
+
+        public MainWindow()
+        {          
+            InitializeComponent();
+
+            this.Title = "NSInstaller v" + getAssemblyVersion();
+
+            // The Installation Folder where we dump the downloaded files.
+            if (!Directory.Exists("temp")) Directory.CreateDirectory("temp");
+        }
+
+        // Credits to XCI-Explorer!
+        private string getAssemblyVersion()
+        {
+            string assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            string[] versionArray = assemblyVersion.Split('.');
+
+            assemblyVersion = string.Join(".", versionArray.Take(3));
+
+            return assemblyVersion;
+        }
+
+        private void openFileLocationBttn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // Credits to Ookii for providing the FolderBrowserDialog
+            // Credits to C. Augusto Proiete and CAD bloke for sorting out the files.
+
+            VistaFolderBrowserDialog dialog = new VistaFolderBrowserDialog();
+            dialog.Description = "Please select the micro sd folder.";
+            dialog.UseDescriptionForTitle = true; // This applies to the Vista style dialog only, not the old dialog.
+            if (!VistaFolderBrowserDialog.IsVistaFolderDialogSupported)
+                MessageBox.Show(this, "Because you are not using Windows Vista or later, the regular folder browser dialog will be used. Please use Windows Vista to see the new dialog.", "Sample folder browser dialog");
+            if ((bool)dialog.ShowDialog(this))
+                filePathTxt.Text = dialog.SelectedPath;                
+        }
+
+        private void startBttn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (filePathTxt.Text.Length == 0) MessageBox.Show("Please select the micro sd folder before starting the installation.", "NSInstaller", MessageBoxButton.OK, MessageBoxImage.Warning);
+            else if (filePathTxt.Text.Length > 5)
+            {
+                setAllButtonIsEnabled(false);
+                startBttn.Content = "Installing...";
+                
+                Util util = new Util();               
+                for (var i = 0; i < 100; i++)
+                {                    
+                    progressBar.Value += i;
+                    
+                    if (progressBar.Value == 100) break;                    
+                }
+            }
+        }
+        private void installSigBttn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void fixAtmoBttn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void updtHekateBttn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
+
+        private void updtAtmoBttn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }        
+
+        private void setAllButtonIsEnabled(bool isEnabled)
+        {
+            filePathTxt.IsEnabled = isEnabled;
+            openFileLocationBttn.IsEnabled = isEnabled;
+            startBttn.IsEnabled = isEnabled;
+            installSigBttn.IsEnabled = isEnabled;
+            fixAtmoBttn.IsEnabled = isEnabled;
+            updtHekateBttn.IsEnabled = isEnabled;
+            updtAtmoBttn.IsEnabled = isEnabled;
+        }
+    }
+}
---
+++ NSInstaller/Utils/Util.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NSInstaller.Utils
+{
+    class Util
+    {
+        // Credits to CTCaer & Contributors for making this amazing Bootloader!
+        public string hekate_releases = "https://api.github.com/repos/CTCaer/hekate/releases";
+
+        // Credits to SciresM & Contributors for making this amazing Custom Firmware!
+        public string atmosphere_releases = "https://api.github.com/repos/Atmosphere-NX/Atmosphere/releases";
+
+        // Credits to ITotalJustice for providing Signature Patches!
+        public string sig_patches = "https://api.github.com/repos/ITotalJustice/patches/releases";
+    }
+}
